8d1dcfc28e07b6aea5a77e3df035b44fa72f9518
Connie Leung <cleung334@gmail.com>
2026-09-19 16:06:45 +0800

    chore: add route title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
+    title: 'Firebase TTS',
     loadComponent: () => import('./features/dashboard/dashboard.component'),
   },
   {
